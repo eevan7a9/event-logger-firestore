17804f6dc19073278028f06a1ff2e149493723c8
eevan7a9 <e7a9@protonmail.com>
2020-02-28 19:25:01 +0900

    able to show data from firestore

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log(db);
+db.collection("events")
+  .get()
+  .then(snapshot => {
+    // console.log(snapshot.docs); //shows data
+    snapshot.docs.forEach(doc => {
+      console.log(doc.data());
+    });
+  });
